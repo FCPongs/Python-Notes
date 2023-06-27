7dd3a029c90639652296313721efb3026cb045bd
FCPongs <metherelax@gmail.com>
2023-06-27 14:30:14 +0800

    Changed Greetings to Hi

--- (1) VarFunc.py
+++ (1) VarFunc.py
@@ -11,7 +11,7 @@ print(choice)
 
 
 def greeting():
-    return "Greetings! youve called this function!"
+    return "Hi! youve called this function!"
 
 greeting() #! Calling the function but it doesnt print with this code
 response = greeting() #! So we assign it to a variable
